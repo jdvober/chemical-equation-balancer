e645bba43ec0652dfcdcb3dc5582eda360c18316
jvoberMCS <114746137+jvoberMCS@users.noreply.github.com>
2024-02-26 08:16:23 -0500

    up

--- src/components/FormulaEditor/DragAndDrop/Draggable.tsx
+++ src/components/FormulaEditor/DragAndDrop/Draggable.tsx
@@ -19,7 +19,10 @@ DraggableArguments) => {
 		? {
 				transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
 		  }
-		: undefined
+		: {
+				transform: `translate3d(${transform?.x}px, ${transform?.y}px, 0)`,
+		  }
+	// : undefined
 
 	return (
 		<button ref={setNodeRef} style={style} {...listeners} {...attributes}>
