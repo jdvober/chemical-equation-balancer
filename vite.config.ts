6c652013bccda188a0b97161384b31b0ba46857b
jvoberMCS <114746137+jvoberMCS@users.noreply.github.com>
2025-01-07 09:24:31 -0500

    Updating output dir

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig( {
   plugins: [ react(), tsconfigPaths() ],
+  build: {
+    outDir: "../dist/client"
+  },
   server: {
     watch: {
       usePolling: true,
