bd9eba73ca0807e6973c1d0046ff03103d3d7246
jvoberMCS <114746137+jvoberMCS@users.noreply.github.com>
2026-01-23 08:01:40 -0500

    Edit Formula --> Change Formula

--- src/components/Balancer/EditFormulaButton.tsx
+++ src/components/Balancer/EditFormulaButton.tsx
@@ -5,38 +5,38 @@ import React from 'react'
 
 type Props = {}
 type EditFormulaButtonProps =
-	Props extends Record<string, never>
-		? React.FC<Record<string, never>>
-		: React.FC<Props>
+    Props extends Record<string, never>
+    ? React.FC<Record<string, never>>
+    : React.FC<Props>
 
 export const EditFormulaButton: EditFormulaButtonProps = () => {
-	return (
-		<NavLinkButton
-			to={'/editor'}
-			bg={dracCurrentLine}
-			color={dracFG}
-			width="6rem"
-			height="5rem"
-			css={{ transition: 'all 0.1s ease-in-out' }}
-			_hover={{
-				color: 'red',
-				boxShadow: `0 5px 15px purple`,
-			}}
-			borderRadius=".5rem"
-			ml="1rem"
-		>
-			<SimpleGrid>
-				<GridItem>
-					<Center>
-						<Text fontSize="1.25rem">Edit</Text>
-					</Center>
-				</GridItem>
-				<GridItem>
-					<Center>
-						<Text fontSize="1.25rem">Formula</Text>
-					</Center>
-				</GridItem>
-			</SimpleGrid>
-		</NavLinkButton>
-	)
+    return (
+        <NavLinkButton
+            to={'/editor'}
+            bg={dracCurrentLine}
+            color={dracFG}
+            width="6rem"
+            height="5rem"
+            css={{ transition: 'all 0.1s ease-in-out' }}
+            _hover={{
+                color: 'red',
+                boxShadow: `0 5px 15px purple`,
+            }}
+            borderRadius=".5rem"
+            ml="1rem"
+        >
+            <SimpleGrid>
+                <GridItem>
+                    <Center>
+                        <Text fontSize="1.25rem">Change</Text>
+                    </Center>
+                </GridItem>
+                <GridItem>
+                    <Center>
+                        <Text fontSize="1.25rem">Formula</Text>
+                    </Center>
+                </GridItem>
+            </SimpleGrid>
+        </NavLinkButton>
+    )
 }
